6401515ebdbf25793991e40061d7b5585a17305f
Mirage <yangkangxierong@gmail.com>
2017-07-28 19:26:45 +0800

    添加NewtonSoft.Json
    添加NewtonSoft.Json

---
+++ MirageClient/Assets/Game/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Text;
+using System;
+using Newtonsoft.Json.Linq;
+class test
+{
+    public int x = 23;
+    private float y = 345.905f;
+    public float Y { get { return y; } set { y = value; } }
+
+}
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        test t = new test();
+        t.x = 56;
+        t.Y = 123.4546f;
+        string str = JsonConvert.SerializeObject(t);
+
+        test tt = JsonConvert.DeserializeObject<test>(str);
+        Debug.LogError(tt.x+"  ");
+        Debug.LogError(tt.Y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- MirageClient/IdeaList.md
+++ MirageClient/IdeaList.md
@@ -1,1 +1,8 @@
-MirageUIFramework -
+# <font color=0xff>Mirage_UIFramework</font> #
+使用MVVM思想指导实现一套UI框架
+## Unity核心框架部分
+
+## 工具编辑和调试部分
+
+# <font color=0xff>Mirage_WorkFlow</font> #
+不同工作职责的人 实时的数据 创建 修改 传输 合作
